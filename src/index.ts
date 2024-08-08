461b23d5e08050677d51b82e94b4bdf0c6f31fc1
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2024-08-08 17:53:43 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import crypto from 'crypto';
 
-export function encrypt(text, secretKey, algorithm) {
+export function encrypt(text: string, secretKey: string, algorithm: string) {
     const cipher = crypto.createCipher(algorithm, secretKey);
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return encrypted;
 }
 
-export function decrypt(encryptedText, secretKey, algorithm) {
+export function decrypt(encryptedText: string, secretKey: string, algorithm: string) {
     const decipher = crypto.createDecipher(algorithm, secretKey);
     let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
